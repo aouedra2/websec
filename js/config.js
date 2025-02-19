df8e2440ae3e7c115ba45c177fe213e0915ce187
aouedra2 <aouedra2@jh.edu>
2025-02-19 15:45:22 -0800

    updating config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_WUUFs2sSS',
+        userPoolClientId: '5rl3fqsu4nvnl3u08h4kulc3dt',
+        region: 'us-east-2'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
